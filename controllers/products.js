9d734bd1e59ec2bd483da743870e29e5ce266219
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-25 12:58:51 +0530

    Select query implementation

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,11 @@
 const Product=require('../models/product')
 
 const getAllProductsStatic=async (req, res) => {
-  const search='ab'
-  const products = await Product.find({
-    name:{$regex:search, $options:'i'},
-  })
+  const products = await Product.find({}).select('name price')
   res.status (200).json({products,nbHits:products.length})
 }
 const getAllProducts=async (req, res) => {
-  const {featured,company,name}=req.query
+  const {featured,company,name,sort, fields}=req.query
   const queryObject = {}
   if (featured){
     queryObject.featured=featured==='true' ? true :false
@@ -21,8 +18,23 @@ const getAllProducts=async (req, res) => {
   if(name){
     queryObject.name={$regex:name, $options:'i'}
   }
-  console.log(queryObject)
-  const products = await Product.find(queryObject)
+  //console.log(queryObject)
+  let result =  Product.find(queryObject)
+  //sort
+  if (sort){
+    const sortList=sort.split(',').join(' ');
+    result=result.sort(sortList)
+  }
+  else{
+    result=result.sort('createdAt')
+  }
+
+  if(fields){
+    const fieldsList=fields.split(',').join(' ');
+    result=result.select(fieldsList)
+  }
+
+  const products=await result
   res.status(200).json({products,nbHits:products.length})
 }
 
