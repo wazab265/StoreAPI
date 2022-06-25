a3695c8131aeb851b4e6bfb3b31b5a88ce58d36c
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-25 10:54:17 +0530

    Basic query param setup

--- controllers/products.js
+++ controllers/products.js
@@ -2,10 +2,11 @@ const Product=require('../models/product')
 
 const getAllProductsStatic=async (req, res) => {
   const products = await Product.find({})
-  res.status (200).json({products})
+  res.status (200).json({products,nbHits:products.length})
 }
 const getAllProducts=async (req, res) => {
-  res.status(200).json({msg:'products route'})
+  const products = await Product.find(req.query)
+  res.status(200).json({products,nbHits:products.length})
 }
 
 module.exports={
