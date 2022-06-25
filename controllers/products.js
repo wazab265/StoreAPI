7e5e4d5297de0c54e821a945804ff2290652125b
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-25 10:30:24 +0530

    Schema set up and Populating DB

---
+++ controllers/products.js
@@ -0,0 +1,14 @@
+const Product=require('../models/product')
+
+const getAllProductsStatic=async (req, res) => {
+  const products = await Product.find({})
+  res.status (200).json({products})
+}
+const getAllProducts=async (req, res) => {
+  res.status(200).json({msg:'products route'})
+}
+
+module.exports={
+  getAllProducts,
+  getAllProductsStatic,
+}
--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
+
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
