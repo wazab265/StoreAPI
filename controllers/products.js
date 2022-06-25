913e9c86fdb561f3ee97c532f7f66195801fd9eb
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-25 12:03:40 +0530

    Search filtering

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,25 @@
 const Product=require('../models/product')
 
 const getAllProductsStatic=async (req, res) => {
-  const products = await Product.find({})
+  const search='ab'
+  const products = await Product.find({
+    name:{$regex:search, $options:'i'},
+  })
   res.status (200).json({products,nbHits:products.length})
 }
 const getAllProducts=async (req, res) => {
-  const {featured}=req.query
+  const {featured,company,name}=req.query
   const queryObject = {}
   if (featured){
     queryObject.featured=featured==='true' ? true :false
+  }
+
+  if(company){
+    queryObject.company=company
+  }
+
+  if(name){
+    queryObject.name={$regex:name, $options:'i'}
   }
   console.log(queryObject)
   const products = await Product.find(queryObject)
