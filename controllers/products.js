668cd356487d2285d2e65900eab82f18780dcbee
V.Meganathan <meganathan.vjms@gmail.com>
2022-06-30 17:49:47 +0530

    converting logical opertors into mongoose code

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,14 @@
 const Product=require('../models/product')
 
 const getAllProductsStatic=async (req, res) => {
-  const products = await Product.find({})
-  .sort('name')
+  const products = await Product.find({price:{ $gt:30 }})
+  .sort('price')
   .select('name price')
 
   res.status (200).json({products,nbHits:products.length})
 }
 const getAllProducts=async (req, res) => {
-  const {featured,company,name,sort, fields}=req.query
+  const {featured,company,name,sort, fields, numericFilters}=req.query
   const queryObject = {}
   if (featured){
     queryObject.featured=featured==='true' ? true :false
@@ -21,7 +21,24 @@ const getAllProducts=async (req, res) => {
   if(name){
     queryObject.name={$regex:name, $options:'i'}
   }
-  //console.log(queryObject)
+
+  if(numericFilters){
+    const operatorMap={
+      '>':'$gt',
+      '>=':'$gte',
+      '=':'$eq',
+      '<':'$lt',
+      '<=':'$lte',
+    }
+    const regEx=/\b(<|>|>=|=|<|<=)\b/g
+    let filters=numericFilters.replace(
+      regEx,
+      (match)=>`-${operatorMap[match]}-`
+    )
+    console.log(filters)
+  }
+
+  console.log(queryObject)
   let result =  Product.find(queryObject)
   //sort
   if (sort){
